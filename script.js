9ee0163028e73f1a0ff10236d17db95fc047a506
joshika-who <kotagirijoshika@gmail.com>
2026-03-02 20:31:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -452,6 +452,8 @@ function saveToFirebase(photoBase64 = null) {
 }).catch((error) => {
   console.error("Firebase error:", error);
 });
+
+}
 /* ---------------- CONFETTI ---------------- */
 
 function launchConfetti(selectedColor) {
